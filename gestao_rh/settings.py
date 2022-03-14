3d1818be59baea8f3e0c1efef14c9d2b0731dc13
PabloArcuri <36961236+PabloArcuri@users.noreply.github.com>
2022-03-14 11:51:10 -0300

    Update settings.py

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gg$0qt6votyxhcz=t4)%!kcdkqf^mx7)2w0+y7e6cutp#(u*+e'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
